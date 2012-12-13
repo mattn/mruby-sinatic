cc42fa7540d657776d9860074c5c18c12ee332a3
mattn <mattn.jp@gmail.com>
2012-12-13 16:55:17 +0900

    add bbs application.

---
+++ example/bbs.rb
@@ -0,0 +1,47 @@
+#!mruby
+
+db = SQLite3::Database.new("./bbs.db")
+
+get "/" do
+'
+<script src="http://code.jquery.com/jquery-latest.js"></script>
+<script>
+$(function() {
+  function reload() {
+    $.getJSON("/api", function(data) {
+      var ul = $("<ul/>").appendTo($("#list").empty());
+      $.each(data, function() {
+        $("<li/>").text(this[1]).appendTo(ul);
+      });
+    });
+  }
+  $("#post").click(function() {
+    $.post("/api", {text: $("#text").val()}, function() {
+      reload();
+    });
+  });
+  reload();
+});
+</script>
+<label for="text"/>comment:</label>
+<input type="text" id="text" name="text" value="">
+<input id="post" type="submit">
+<div id="list"></div>
+'
+end
+
+get "/api" do |r, param|
+  ret = []
+  db.execute('select * from bbs') do |row|
+    ret += [row]
+  end
+  # TODO: support JSON
+  ret.to_s
+end
+
+post "/api" do |r, param|
+  db.execute_batch('insert into bbs(text) values(?)', param['text'])
+  "true"
+end
+
+Sinatic.run
---
+++ example/bbs.sql
@@ -0,0 +1,1 @@
+create table bbs (id integer primary key, text text);
